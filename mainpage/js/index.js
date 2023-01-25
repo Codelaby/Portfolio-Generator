1bc74e639d072fcddf95f96d7bdcdd1565999152
Codelaby <123108042+Codelaby@users.noreply.github.com>
2023-01-25 11:46:31 +0100

    Fix: Dark Mode

--- mainpage/js/index.js
+++ mainpage/js/index.js
@@ -3,10 +3,20 @@ const navIconEl = document.querySelector(".nav-toggle");
 
 /************ DARK THEM MODE */
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  document.documentElement.setAttribute('data-theme', "dark")
+  document.documentElement.setAttribute('data-theme', "dark");
 } else {
-  document.documentElement.setAttribute('data-theme', "light")
+  document.documentElement.setAttribute('data-theme', "light");
 }
+window.matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', event => {
+    if (event.matches) {
+      //dark mode
+      document.documentElement.setAttribute('data-theme', "dark")
+    } else {
+      //light mode
+      document.documentElement.setAttribute('data-theme', "light");
+    }
+  })
 
 /************ SHOW MENU - MOBILE */
 
